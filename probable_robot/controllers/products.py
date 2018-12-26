c5f4324f94326fafa9f65c7cf2da2275d84e35c0
Phil Goo Kang <philgookang@gmail.com>
2018-12-26 13:31:22 +0900

    added product view detail page

--- probable_robot/controllers/products.py
+++ probable_robot/controllers/products.py
@@ -15,6 +15,9 @@ def index(request):
     ml_exclude_price   = request.GET.get('ml_exclude_price', None)
     ml_exclude_lang    = request.GET.get('ml_exclude_lang', None)
     hide_orgi_name     = request.GET.get('hide_orgi_name', None)
+    brand_idx          = request.GET.get('brand_idx', None)
+    category_idx       = request.GET.get('category_idx', None)
+    level              = request.GET.get('level', None)
     search_name        = request.GET.get('search_name', '')
     search_name_ori    = request.GET.get('search_name_ori', '')
 
@@ -41,8 +44,13 @@ def index(request):
     pm = ProductsM()
     if ml_exclude_price == '1': pm.ml_exclude_price = 0
     if ml_exclude_lang == '1':  pm.ml_exclude_lang  = 0
+    if brand_idx:  pm.brand_idx = brand_idx
     if search_name != '': pm.search_name = search_name
     if search_name_ori != '': pm.search_name_ori = search_name_ori
+    if level == '0' and category_idx != None:
+        pm.category_1_idx = category_idx
+    elif level == '1' and category_idx != None:
+        pm.category_2_idx = category_idx
     product_list = pm.getList( offset=(int(page)*int(limit)), limit=limit  )
     product_list = list(map(retrieveMore, product_list))
     total_count = pm.getTotal()
@@ -73,9 +81,40 @@ def index(request):
         'ml_exclude_price'  : ml_exclude_price,
         'ml_exclude_lang'   : ml_exclude_lang,
         'hide_orgi_name'    : hide_orgi_name,
+        'brand_idx'         : brand_idx,
+        'level'             : level,
+        'category_idx'      : category_idx,
         'search_name'       : search_name,
         'search_name_ori'   : search_name_ori,
         'pagination'        : pagination(total_count['cnt'], page, limit)
     }
     template = loader.get_template('products/product_list.html')
+    return HttpResponse(template.render(data, request))
+
+
+def view(request, *args, **kwargs):
+
+    if 'product_idx' not in kwargs:
+        return HttpResponse("Missing parameter")
+
+    # -------
+
+    product = ProductsM()
+    product.idx = kwargs['product_idx']
+    product = product.get()
+
+    # brand information
+    pb = ProductBrandsM()
+    pb.idx = product['brand_idx']
+    product['brand'] = pb.get()
+
+    # image list
+    pi = ProductImagesM()
+    pi.product_idx = product['idx']
+    product['image_list'] = pi.getList(nolimit = True)
+
+    data = {
+        'product' : product
+    }
+    template = loader.get_template('products/product_view.html')
     return HttpResponse(template.render(data, request))
